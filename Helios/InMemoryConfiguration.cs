78cec1786cf7b792d404c3db1b8dabcc7455dd30
Unknown <shandy1806@gmail.com>
2018-02-23 17:06:20 +0200

    init identity server

--- Helios/InMemoryConfiguration.cs
+++ Helios/InMemoryConfiguration.cs
@@ -1,7 +1,44 @@
-﻿namespace Helios
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Helios
 {
     public class InMemoryConfiguration
     {
-        
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("sun", "Sun")
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "sun",
+                    ClientSecrets = new[] {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "sun" }
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "shani@gmail.com",
+                    Password = "password"
+                }
+            };
+        }
     }
 }
